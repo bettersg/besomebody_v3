b1f9f0411f137d001d7c8575f2bb62d6ca48793e
youxuanpeng <54617188+youxuanpeng@users.noreply.github.com>
2021-06-21 23:55:31 +0800

    merge

--- src/pages/DefaultInk.js
+++ src/pages/DefaultInk.js
@@ -4,18 +4,22 @@ import NextButton from "../components/NextButton"
 
 
 const DefaultInk = (props) => {
-  const { currentParagraphs, getStory, choices, setChoice, specialTags } = props
+  const { currentParagraphs } = props
+  const { getStory, choices, setChoice } = useInkContext()
+  const step = currentParagraphs[currentParagraphs.length - 1]
+  console.log(step)
+
   return (
-    <Box >
-      {currentParagraphs?.map((paragraph) => {
-        return (
-          <Box my={2}>
-            <Fade in={paragraph.text}>
-              <Typography align="center">{paragraph.text}</Typography>
-            </Fade>
-          </Box>
-        )
-      })}
+    <Box className="DefaultInk">
+      {currentParagraphs.length>0 ?
+        <Box my={2}>
+          <Fade in={step.text}>
+            <Typography align="center" className="DefaultInk__text">{step.text}</Typography>
+          </Fade>
+        </Box>
+        :
+        null
+      }
 
       {choices?.map((choice) => {
         return (
