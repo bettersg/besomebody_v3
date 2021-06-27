4016e0f45080644a96337567877e641cd4d30165
Ravern Koh <ravernkoh@gmail.com>
2021-06-27 16:04:37 +0800

    add "Story complete" screen

--- src/pages/ReflectionsPage/Reflection.js
+++ src/pages/ReflectionsPage/Reflection.js
@@ -4,7 +4,8 @@ import REFLECTIONS from "../../reflections/reflections.json"
 import ReflectionForm from './shared/ReflectionForm';
 import ChapterReflectionResponses from './chapter/ChapterReflectionResponses';
 import ChapterComplete from './chapter/ChapterComplete';
-import { REFLECTION_PAGE_FORM, REFLECTION_PAGE_CHAPTER_COMPLETE, REFLECTION_PAGE_CHAPTER_REFLECTION_RESPONSES } from './constants';
+import StoryComplete from './story/StoryComplete';
+import { REFLECTION_PAGE_FORM, REFLECTION_PAGE_STORY_COMPLETE, REFLECTION_PAGE_CHAPTER_COMPLETE, REFLECTION_PAGE_CHAPTER_REFLECTION_RESPONSES } from './constants';
 
 const Reflection = ({ getStory, reflectionId }) => {
   const [page, setPage] = useState(REFLECTION_PAGE_CHAPTER_COMPLETE);
@@ -23,6 +24,8 @@ const Reflection = ({ getStory, reflectionId }) => {
       return <ChapterComplete setPage={setPage} />
     case REFLECTION_PAGE_CHAPTER_REFLECTION_RESPONSES:
       return <ChapterReflectionResponses reflectionId={reflectionId} setPage={setPage} />
+    case REFLECTION_PAGE_STORY_COMPLETE:
+      return <StoryComplete setPage={setPage} />
     case REFLECTION_PAGE_FORM:
       return <ReflectionForm reflection={reflection} />;
     default:
