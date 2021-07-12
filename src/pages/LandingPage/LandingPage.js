3b8e5f62a8e2d17f95254b78132c1c2c19332414
gauravkeerthi <gauravkeerthi@gmail.com>
2021-07-12 11:03:08 +0800

    Starting landing sequence

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -11,67 +11,106 @@ import {
   Typography,
   Container,
 } from '@material-ui/core'
-import { Alert } from '@material-ui/lab'
+// import { Alert } from '@material-ui/lab'
 import { useAuth } from '../../contexts/AuthContext'
-import { Link } from 'react-router-dom'
+import { Link , useHistory} from 'react-router-dom'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+
+const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundImage: ({ image }) => `url('/images/bg_launch.png')`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    [theme.breakpoints.up('md')]: {
+      height: '660px',
+    },
+    bottom: 0, 
+
+  },
+  bottom: {
+    bottom: 0,
+    height: '20vh',
+    position: 'absolute',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    left: 0,
+    right:0,
+    textAlign: 'center',
+    // display: 'flex',    
+    // flexDirection: 'column',
+    // alignItems: 'center',
+  },
+  container: {
+    margin: 'auto',
+    textAlign: 'center',
+  },
+  btn: {
+    padding: '10px 50px',
+    borderRadius: '40px',
+    marginBottom: '20px',
+    background: '#664EFC',
+    backgroundColor: '#664EFC',
+    textDecoration: 'none',
+    color: '#ffffff',
+    fontWeight: '700',
+    '&:hover': {
+      backgroundColor: '#6C70DD',      
+      boxShadow: 'none',
+      
+    },
+  },
+  link: {
+    color: '#000A11',
+    fontSize: '0.9rem',
+    fontWeight: '700',
+    textDecoration: 'none',
+    '&:hover': {
+      color: '#664EFC'
+    },
+  }
+}))
 
 const LandingPage = () => {
   const [isLoading, setIsLoading] = useState(false)
-  const [isDialogOpen, setIsDialogOpen] = useState(false)
-  const [snackbar, setSnackbar] = useState({
-    message: '',
-    open: false,
-    type: 'error',
-  })
+  // const [isDialogOpen, setIsDialogOpen] = useState(false)
+  // const [snackbar, setSnackbar] = useState({
+  //   message: '',
+  //   open: false,
+  //   type: 'error',
+  // })
+  const classes = useStyles()  
+  const history = useHistory()
 
-  const { currentUser, logout, deleteUser } = useAuth()
-
-  const handleLogout = async () => {
-    try {
-      setIsLoading(true)
-      await logout()
-    } catch (err) {
-      setSnackbar({
-        message: `Failed to logout: ${err.message}`,
-        open: true,
-        type: 'error',
-      })
-    }
-
-    setIsLoading(false)
-  }
- 
+  const { currentUser } = useAuth()  
 
   return (
-    <Box>
-      <section>
-        <Container maxWidth="md">
-          <Box py={8} textAlign="center">
-            <Typography variant="h3" component="h2" gutterBottom={true}>To Be You</Typography>
-            <Typography variant="h6" color="textSecondary" paragraph={true}>This is the landing page for https://game.tobeyou.sg. Players who sign up on the pre-launch page will be given this link for them to start playing the game.</Typography>
+    <Box className={classes.background}>
+      
+
+      
+      
+        <Container maxWidth="md" className={classes.container}>
+          <Box py={4} textAlign="center">
+          <Typography className='LandingPage__text'>Experience life in Singapore as someone else.</Typography>            
           </Box>
-          <Box mx="auto" width="75%" my={2}>
-            <Link to="/intro"><Button variant="contained" fullWidth={true} color="primary"  >Start New Game</Button></Link>
-          </Box>
-          <Box mx="auto" width="75%" my={2}>
-            <Link to="/login"><Button variant="contained" fullWidth={true} color="secondary" >Login to Resume Game</Button></Link>
-            
+          
+          
+          <Box className={classes.bottom}>
+            <Button variant="contained" className={classes.btn}  onClick={() => history.push('/intro')}>Start New Game</Button>          
+          <br />
+           <Link to="/login" className={classes.link}> Login to Resume</Link>
           </Box>
           
         </Container>
-      </section>
+       
       
       
  
  
 
-      <Snackbar
-        open={snackbar.open}
-        autoHideDuration={6000}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      >
-        <Alert severity={snackbar.type}>{snackbar.message}</Alert>
-      </Snackbar>
+      
     </Box>
   )
 }
@@ -80,15 +119,14 @@ export default LandingPage;
 
 /*  
 
-        <Box maxWidth="md" mx="auto" display="flex" justifyContent="center">
-          <Button
-            color="secondary"
-            variant="outlined"
-            disabled={isLoading}
-            onClick={handleLogout}
-          >
-            Logout
-        </Button>
-        </Box>
+        
+
+        <Box mx="auto" width="75%" my={2}>
+            <Link to="/intro"><Button variant="contained" fullWidth={true} color="primary"  >Start New Game</Button></Link>
+          </Box>
+          <Box mx="auto" width="75%" my={2}>
+            <Link to="/login"><Button variant="contained" fullWidth={true} color="secondary" >Login to Resume Game</Button></Link>
+            
+          </Box>
         
 */
