0c873d7a5b7edcdd19f7b47f3ed8323846ce1e32
youxuanpeng <54617188+youxuanpeng@users.noreply.github.com>
2021-07-16 01:15:14 +0800

    update reflection ui (contained in page)

--- src/pages/ReflectionsPage/chapter/ChapterReflectionResponses.js
+++ src/pages/ReflectionsPage/chapter/ChapterReflectionResponses.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Button, CircularProgress } from '@material-ui/core'
+import makeStyles from '@material-ui/core/styles/makeStyles'
 
 import { getDbReflectionResponses } from '../../../models/reflectionResponseModel';
 import { REFLECTION_PAGE_FORM } from '../constants';
 import ChapterResponse from './ChapterResponse';
-
+const useStyles = makeStyles((theme) => ({
+  paragraphWrapper: {
+    backgroundColor: "white", 
+    height: '660px',
+    [theme.breakpoints.only('xs')]: {
+        height: '100vh',
+    },
+    bottom: 0, 
+    overflow: "auto",
+  },
+}))
 const ChapterReflectionResponses = ({ reflectionId, setPage }) => {
   const [responses, setResponses] = useState(null);
 
@@ -14,17 +25,18 @@ const ChapterReflectionResponses = ({ reflectionId, setPage }) => {
       questionId: 3,
     }).then(setResponses).catch(console.error);
   }, [reflectionId, setResponses]);
+  const classes = useStyles() 
 
   if (responses == null) {
     return <CircularProgress />;
   } else {
     return <>
-      <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+      <Box  className={classes.paragraphWrapper} flexDirection="column" justifyContent="center" alignItems="center">
         {responses.map(response => (
           <ChapterResponse key={response.id} response={response} />
         ))}
+        <Button variant="contained" onClick={() => setPage(REFLECTION_PAGE_FORM)}>Add your own response</Button>
       </Box>
-      <Button variant="contained" onClick={() => setPage(REFLECTION_PAGE_FORM)}>Add your own response</Button>
     </>;
   }
 }
