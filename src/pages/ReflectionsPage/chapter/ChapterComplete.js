5cc0e3120eb8ca2c3a0440e0e9e0e23a2f659322
youxuanpeng <54617188+youxuanpeng@users.noreply.github.com>
2021-07-16 01:11:05 +0800

    update chapter complete ui

--- src/pages/ReflectionsPage/chapter/ChapterComplete.js
+++ src/pages/ReflectionsPage/chapter/ChapterComplete.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Box, Button } from '@material-ui/core'
+import { Box, Button, Typography } from '@material-ui/core'
+import makeStyles from '@material-ui/core/styles/makeStyles'
 
 import { REFLECTION_PAGE_CHAPTER_REFLECTION_RESPONSES } from '../constants';
+const useStyles = makeStyles((theme) => ({
+  paragraphWrapper: {
+    backgroundColor: "white", 
+    height: '660px',
+    [theme.breakpoints.only('xs')]: {
+        height: '100vh',
+    },
+    bottom: 0, 
+  },
+}))
 
 const ChapterComplete = ({ setPage }) => {
-  return <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-    Chapter complete!
+  const classes = useStyles() 
+
+  return <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" className={classes.paragraphWrapper} >
+    <Typography>Chapter complete!</Typography> 
+    <br/>
     <Button variant="contained" color="primary" onClick={() => setPage(REFLECTION_PAGE_CHAPTER_REFLECTION_RESPONSES)}>
       Continue
     </Button>
