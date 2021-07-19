d86357a1321383ca149a37cf9ba7ec3ead6941d8
gauravkeerthi <gauravkeerthi@gmail.com>
2021-07-19 17:08:23 +0800

    update to intro text

--- src/pages/LandingPage/OpeningSequence.js
+++ src/pages/LandingPage/OpeningSequence.js
@@ -79,7 +79,7 @@ const introParagraphs = {
     "choices":null,
   },
   { step:5,
-    "text": "To play the game, we'll need to create an account for you using your email address and a password. We will only use this to save your game, and we won't use it for anything else.",
+    "text": "To play the game, we'll need to create an account for you using your email address and a password. We will  use this to save your game, and if we happen to find interesting trends, we will share insights that we discover! No matter what, this data will never be used to identify you!",
     "choices":null,
   },
 ]}
