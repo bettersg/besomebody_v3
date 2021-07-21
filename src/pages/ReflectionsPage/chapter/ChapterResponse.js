9638cc3ceb00384737420dc2baf5a69ead2ae54b
gauravkeerthi <gauravkeerthi@gmail.com>
2021-07-21 20:09:52 +0800

    added quick and dirty red flag

--- src/pages/ReflectionsPage/chapter/ChapterResponse.js
+++ src/pages/ReflectionsPage/chapter/ChapterResponse.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Typography, Box } from '@material-ui/core'
+import { Button, Typography, Box , Grid } from '@material-ui/core'
 import makeStyles from '@material-ui/core/styles/makeStyles'
+import FlagIcon from '@material-ui/icons/Flag';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
+import { useAuth } from '../../../contexts/AuthContext'
+
 
 const useStyles = makeStyles((theme) => ({
   reflectionBox: {
@@ -22,16 +27,58 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 400,
     fontSize: 12,
   },
+  flag: {
+    color: '#E2E2F8',
+    '&:hover': {
+      color: '#ff0000',
+      cursor: 'pointer',
+    },
+  }
 }))
 
 
 
 const ChapterResponse = ({ response }) => {
   const classes = useStyles()
+  const { currentUser } = useAuth()
+
+  const responseMessage = response.answer + ' | reflection ID: ' + response.reflectionId + ' | user ID : ' +   response.userId + ' | submitted at ' + response.submittedAt 
+
+  const [toSend, setToSend] = useState({
+    from_name: currentUser.email,
+    to_name: 'TOBEYOU ADMIN',
+    message: responseMessage,
+    reply_to: 'tobeyou@better.sg',
+  });
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_mxyo80c',
+      'template_r9ys0jd',
+      toSend,
+      'user_kmfKhjRSSwoovXNarQivp'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  // const handleChange = (e) => {
+  //   setToSend({ ...toSend, [e.target.name]: e.target.value });
+  // };
+
   return (
     <Box className={classes.reflectionBox}>
       <Typography className={classes.storyText}>{response.answer} </Typography> <br/>
-      <Typography className={classes.demographicsText}>~{response.user.age? response.user.age + ' YRS OLD' : null} {response.user.race? ' | ' + response.user.race : null }  {response.user.religion? ' | ' + response.user.religion : null }   {response.user.gender? ' | ' + response.user.gender : null }  {response.user.housing? ' | ' + response.user.housing : null }</Typography>
+      <Grid container >
+        <Grid item xs={9}><Typography className={classes.demographicsText}>~{response.user.age ? response.user.age + ' YRS OLD' : null} {response.user.race ? ' | ' + response.user.race : null}  {response.user.religion ? ' | ' + response.user.religion : null}   {response.user.gender ? ' | ' + response.user.gender : null}  {response.user.housing ? ' | ' + response.user.housing : null}</Typography></Grid>
+        <Grid item xs={2}><FlagIcon className={classes.flag} onClick={onSubmit}/></Grid>
+      </Grid>
     </Box>
   )
 };
