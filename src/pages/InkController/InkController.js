fc00a5facbeca342e124b41ceb4d2dffc49d46a9
gauravkeerthi <gauravkeerthi@gmail.com>
2021-08-29 10:50:14 +0800

    included a call in getUI to the story_end sequence, and a specialtag in the ink story for it
    TODO: This branch does not have the save game functions which are on the live branch, so it must have been pulled from an older branch. Need to ensure that the game saves properly, so that the player can record achievements and move to the next chapter.

--- src/pages/InkController/InkController.js
+++ src/pages/InkController/InkController.js
@@ -33,13 +33,13 @@ const getUi = ({
     case 'notification': {
       return <Notification currentParagraphs={currentParagraphs} />
     }  
-    case 'survey': {
-      // TODO: update this component
-      return <Survey currentParagraphs={currentParagraphs} />
-    }
+    // case 'survey': {
+    //   // TODO: update this component
+    //   return <Survey currentParagraphs={currentParagraphs} />
+    // }
 
     case 'chapter_reflection': {
-      // TODO: update this component
+       
       return (
         <Reflection
           reflectionId={specialTags.reflection_id}
@@ -47,24 +47,37 @@ const getUi = ({
         />
       )
     }
+      
+    case 'story_end': {
+       
+      return (
+        <StoryEnd
+          reflectionId={specialTags.reflection_id}
+          globalVariables={globalVariables}
+        />
+      )
+    }
+      
+      
+      
 
     case 'mcq': {
       return <MultipleChoiceQuiz quizId={specialTags.game_id} />
     }
 
-    case 'swipe': {
-      return <SwipeQuiz quizId={specialTags.game_id} />
-    }
+    // case 'swipe': {
+    //   return <SwipeQuiz quizId={specialTags.game_id} />
+    // }
 
     // case reflection  - return a reflection component with argument for survey id from ink
     // <Reflection getstory surveyid />
 
-    case 'school': {
-      return (
-        // to remove school from nadia's story
-        <Scene currentParagraphs={currentParagraphs} />
-      )
-    }
+    // case 'school': {
+    //   return (
+    //     // to remove school from nadia's story
+    //     <Scene currentParagraphs={currentParagraphs} />
+    //   )
+    // }
     default:
       return <DefaultInk currentParagraphs={currentParagraphs} />
   }
