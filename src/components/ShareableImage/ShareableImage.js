f7ec95e83c112d29bac06c8960d8bc16ddb55b1e
sqiyan <sqiyan@gmail.com>
2021-09-18 20:01:31 +0800

    add contextual Share button function
    on mobile browsers, Share initiates native sharing functionality, on desktop, Share downloads the image. in cases where webshare API doesn't work, Share downloads the Shareable image on default.
    
    NOTE: experiencing a lag in download pop-up when clicking share on desktop, ~35 seconds

--- src/components/ShareableImage/ShareableImage.js
+++ src/components/ShareableImage/ShareableImage.js
@@ -1,6 +1,7 @@
 import GeneratedImage from './GeneratedImage'
 import * as htmlToImage from 'html-to-image';
 import downloadjs from "downloadjs";
+import {isMobile} from 'react-device-detect';
 
 // format which Shareable image takes in data (dictionary):
 // const data = {
@@ -14,20 +15,63 @@ const ShareableImage = ({data,  }) =>{
 
     var exportData = document.getElementsByClassName('htmlToImageVis')
 
-    for (var i = 0; i < exportData.length; ++i){
+    if (isMobile) {
 
-        htmlToImage.toPng(exportData[i])
-          .then(function (dataUrl) {
-            downloadjs(dataUrl, 'to-be-you-shared.png');
-          });
-      }
+      htmlToImage.toBlob(exportData[0])
+        .then(function (blob) {
+          var file = new File([blob], 'to-be-you-shared.png', { type: blob.type })
+          console.log(file)
+          const filesArray = [file]
+          console.log(filesArray)
+          return filesArray
+        })
+        .then((filesArray) => {
+          if (navigator.canShare && navigator.canShare({ files: filesArray })) {
+
+            console.log("files array:"+filesArray)
+    
+            navigator.share({
+                title: "Nadia's Story | To Be You",
+                text: `I've just finished playing Chapter One of Nadia's Story!`,
+                url: document.location.href,
+                files: filesArray
+              })
+              .then(() => {
+                console.log('Successfully shared');
+              })
+              .catch(error => {
+                console.error('Something went wrong sharing the image', error);
+              });
+          }
+          else {
+
+            htmlToImage.toPng(exportData[0])
+              .then(function (dataUrl) {
+                downloadjs(dataUrl, 'to-be-you-shared.png');
+              });
+
+          }
+        });
+
+    }
+
+    else {
+
+      console.log("downloading image..") // to remove once lag issue is resolved
+      htmlToImage.toPng(exportData[0])
+            .then(function (dataUrl) {
+              downloadjs(dataUrl, 'to-be-you-shared.png');
+              console.log("finished download!") // to remove once lag issue is resolved
+            });
+
+    }
 
   }
 
   return <div>
       <h1>Share your experience playing To Be You!</h1>
       <GeneratedImage imageData={data} className="htmlToImageVis"/>
-        <button onClick={exportAsPicture}>Download</button>
+        <button onClick={exportAsPicture}>Share</button>
   </div>
 };
 
