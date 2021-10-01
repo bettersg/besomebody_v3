cb3c398610788ada202511557892f0cb658a018b
gauravkeerthi <gauravkeerthi@gmail.com>
2021-10-01 18:35:58 +0800

    updates

--- src/pages/ShareableImage/ShareableImageContainer.js
+++ src/pages/ShareableImage/ShareableImageContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ShareableImage from './ShareableImage'
 import * as htmlToImage from 'html-to-image';
 import downloadjs from "downloadjs";
-import {isMobile} from 'react-device-detect';
+// import {isMobile} from 'react-device-detect';  // for debug
 import "./style.scss"
 
 // format which Shareable image takes in data (dictionary):
@@ -12,21 +12,35 @@ import "./style.scss"
 //     avatar: 'nadia'}
 
 const ShareableImageContainer = ({data }) =>{
-  const {storyName, text, avatar, avatarImage} = data;
+  const { storyName, text, avatar, avatarImage } = data;
+  const isMobile = true;  // for debug
 
   const exportAsPicture = () => {
 
-    var exportData = document.getElementsByClassName('htmlToImageVis')
+    var exportData = document.getElementsByClassName('ShareableImage') // this is the problem. the element htmlToImageVis is null
+    // console.log('exportdata ', exportData);
+    var exportOptions = {
+      width: 300,
+      height: 300,
+      canvasWidth: 300,
+      canvasHeight: 300  
+    }
+
+    console.log('exportdata[0]', exportData[0]);
 
     if (isMobile) {
 
-      htmlToImage.toBlob(exportData[0])
-        .then(function (blob) {
-          var file = new File([blob], 'to-be-you-shared.jpg', { type: 'image/jpeg' })
-          console.log(file)
-          const filesArray = [file]
-          console.log(filesArray)
-          return filesArray
+      htmlToImage.toJpeg(exportData[0], exportOptions) // why not use htmlToImage.toJpeg(exportData[0],exportOptions)
+        .then(function (dataUrl) {
+          // var file = new File([blob], 'to-be-you-shared.jpg', { type: 'image/jpeg' })
+          // console.log(file)
+          // const filesArray = [file]
+          // console.log(filesArray)
+          // return filesArray
+          var link = document.createElement('a');
+          link.download = 'my-image-name.jpeg';
+          link.href = dataUrl;
+          link.click();
         })
         .then((filesArray) => {
           if (navigator.canShare && navigator.canShare({ files: filesArray })) {
@@ -72,8 +86,8 @@ const ShareableImageContainer = ({data }) =>{
   }
 
   return <div>
-      <h1>Share your experience playing To Be You!</h1>
-      <ShareableImage imageData={data} className="htmlToImageVis"/>
+      <h1 className="testingThisOut">Share your experience playing To Be You!</h1>
+      <ShareableImage imageData={data} className="htmlToImageVis" id="htmlToImageVis"/>
       <div className="ShareableImageBorder"></div>
       <button onClick={exportAsPicture}>Share</button>
   </div>
