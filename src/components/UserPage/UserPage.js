8c808b32fdb749e02126283e054558017e2f6fd9
gauravkeerthi <gauravkeerthi@gmail.com>
2022-10-23 16:27:56 +0800

    removed references to user collection email field

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -170,8 +170,8 @@ function UserPage() {
         </div>
         <div className={classes.accountMenu}>
           <MenuItem logo="/account_page/profile.png" name="Your Profile" description={`${user.age ? user.age + ", " : ""}${user.gender ? user.gender + ", " : ""}${user.race ? user.race + ", " : ""}${user.religion ? user.religion + ", " : ""}${user.housing ? user.housing + ", " : ""}`} editRedirect="/profilebuilder"/>
-          {/* <MenuItem logo="/account_page/email.png" name="Email" description={`${user.email ? user.email : ""}`} /> // replaced this with currentUser instead */  } 
-          <MenuItem logo="/account_page/email.png" name="Email" description={`${currentUser.email ? currentUser.email : ""}`} />
+           {/* <MenuItem logo="/account_page/email.png" name="Email" description={`${user.email ? user.email : ""}`} />  */}
+          <MenuItem logo="/account_page/email.png" name="Email" description={`${currentUser.email ? currentUser.email : ""}`} /> { /* replaced user.email to currentUseremail */ }
           <MenuItem logo="/account_page/password.png" onClick={() => handleResetPassword()} name="Reset Password"/>
           <MenuItem logo="/account_page/logout.png" onClick={logoutUser} name="Logout"/>
         </div>
